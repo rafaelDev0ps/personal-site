29afa923e5a5b8df60fc97261158fab69ead9882
rafaelDev0ps <ra.fa.mt@hotmail.com>
2023-02-24 10:01:16 -0300

    Solve export problem

--- pages/[uid].js
+++ pages/[uid].js
@@ -3,26 +3,29 @@ import { createClient } from '../prismicio'
 import { PrismicRichText } from "@prismicio/react";
 
 export default function Post({ page }) {
-    console.log(page)
     return (
       <>
         <div className="px-40 mx-40 ">
-            <PrismicRichText 
+          { page && (
+            <>
+              <PrismicRichText 
                 field={page.data.title}
                 components={{
                     paragraph: ({ children }) => <h2 className="font-bold text-[32px] mb-16">{children}</h2>
                 }}
-            />
-            <div className='text-xl'>
+              />
+              <div className='text-xl'>
                 <PrismicRichText 
-                    field={page.data.content} 
-                    components={{
-                        paragraph: ({ children }) => <p className="my-10 font-light">{children}</p>, 
-                        image: ({ children }) => {children},
-                        preformatted: ({ children }) => <pre className='text-sm w-full mb-10 max-h-[500px] overflow-auto bg-slate-200 p-2'>{children}</pre>,
-                    }}
+                  field={page.data.content} 
+                  components={{
+                    paragraph: ({ children }) => <p className="my-10 font-light">{children}</p>, 
+                    image: ({ children }) => {children},
+                    preformatted: ({ children }) => <pre className='text-sm w-full mb-10 max-h-[500px] overflow-auto bg-slate-200 p-2'>{children}</pre>,
+                  }}
                 />
-            </div>
+              </div>
+            </>
+          )}
         </div>
         
       </>
--- pages/articles.js
+++ pages/articles.js
@@ -13,7 +13,6 @@ export default function Articles({ page }) {
             ))}
           </div>
       </main>
-      
     </div>
   );
 }
