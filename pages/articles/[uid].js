bde9be8aee83277e4640b171796d19f605f6238a
rafaelDev0ps <ra.fa.mt@hotmail.com>
2023-02-24 14:22:21 -0300

    Fix text and image formatting

--- pages/articles/[uid].js
+++ pages/articles/[uid].js
@@ -24,10 +24,15 @@ export default function Post({ page }) {
                 <PrismicRichText 
                   field={page.data.content} 
                   components={{
+                    heading2: ({ children }) => <h2 className="mt-12 mb-3 font-normal text-3xl">{children}</h2>,
                     heading3: ({ children }) => <h3 className="mt-5 mb-3 font-normal">{children}</h3>,
                     paragraph: ({ children }) => <p className="my-7 font-normal">{children}</p>, 
                     image: ({ children }) => {children},
                     preformatted: ({ children }) => <pre className='text-sm w-full mb-10 max-h-[500px] overflow-auto bg-slate-200 p-2'>{children}</pre>,
+                    hyperlink: ({ children }) => <a className='links'>{children}</a>,
+                    strong: ({ children }) => <strong className='bold-font'>{children}</strong>,
+                    listItem: ({ children }) => <li className='list-disc ml-10'>{children}</li>,
+                    
                   }}
                 />
               </div>
